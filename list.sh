5e521759925ec0fe976c3937d01a674d08865bcf
Matthew Hansen <hypergo@mattian.com>
2024-10-30 15:48:45 -0400

    charts working with table data natively

--- list.sh
+++ list.sh
@@ -6,9 +6,12 @@ dir="${1:-.}"
 # Function to process each directory
 process_directory() {
     local current_dir="$1"
+    # Markdown friendly prompt at the top of the output
+    echo "The following is an application. After you have reviewed it, do not respond except to acknowledge receipt of the information, after which I will provide instructions"
+    echo ""
     find "$current_dir" -type f \( \
         -name '*.py' -o -name '*.html' -o -name '*.js' -o -name '*.css' -o \
-        -name '*.yaml' -o -name '*.txt' -o -name '*.sql' \) \
+        -name '*.yaml' -o -name '*.md' -o -name '*.txt' -o -name '*.sql' \) \
         -exec echo "" \; \
         -exec echo "" \; \
         -exec echo "### File: {}" \; \
